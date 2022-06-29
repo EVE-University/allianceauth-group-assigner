bcb128bee298529140ec5fc7bb7e0db975c4d159
Bates Larsson <Robertgrounds@hotmail.com>
2022-06-30 00:16:48 +0100

    Added state gate
    Added state gate to only allow "member" to "other" on state change to apply group.
    A future upgrade to select gate path.

--- groupassign/signals.py
+++ groupassign/signals.py
@@ -15,11 +15,12 @@ def state_change(sender, instance, raw, using, update_fields, **kwargs):
         if instance.pk:
             old_instance = UserProfile.objects.get(pk=instance.pk)
             if old_instance.state != instance.state:
-                logger.debug("State changed: Checking for Groups")
-                group_lists = StateGroupBinding.objects.get(state=instance.state).groups.all()
-                for grp in group_lists:
-                    if grp not in instance.user.groups.all():
-                        instance.user.groups.add(grp)
+                if old_instance.state.name == "Member":
+                    logger.debug("Uni State changed from member: Checking for Groups")
+                    group_lists = StateGroupBinding.objects.get(state=instance.state).groups.all()
+                    for grp in group_lists:
+                        if grp not in instance.user.groups.all():
+                            instance.user.groups.add(grp)
     except ObjectDoesNotExist as e:
         pass # no config for this state so skip.
     except Exception as e:
